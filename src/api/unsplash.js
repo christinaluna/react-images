00da8ffeed6722b55002567954d179bdab7cfd16
Cristina Luna <christina.luna@meltmedia.com>
2019-06-25 15:34:32 -0700

    Update api key var

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const mykey = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.API_KEY;
 
 export default axios.create( {
 
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID ' + mykey
+        Authorization: 'Client-ID ' + API_KEY
     }
 });
